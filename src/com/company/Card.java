fe96665b4bc5863e2342fd052ed2f434b3d56f5e
BT_1N3_21@bktech.local <BT_1N3_21@bktech.local>
2018-12-13 11:00:51 -0500

    Initial commit

---
+++ src/com/company/Card.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Card {
+    String rank;
+    String suit;
+    int pointValue;
+
+    public Card (String rank, String suit, int pointValue) {
+        this.rank = rank;
+        this.suit = suit;
+        this.pointValue = pointValue;
+    }
+
+    public String getRank(){
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
+
+    public boolean equals(Card otherCard) {
+        return this.pointValue == otherCard.pointValue;
+    }
+
+    public String toString(){
+        return "The card is the " + rank + " of " + suit + "with " + pointValue + " points!";
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] suits = {"Diamonds","Clubs","Hearts","Spades"};
+        String[] ranks = {"1","2","3","4"};
+        int[] pointValues = {1,2,3,4};
+        Deck deck = new Deck(suits,ranks,pointValues);
+        System.out.println(deck.getUnDealt());
+        System.out.println(deck.getDealt());
+        deck.shuffle();
+        while(deck.size() > 0) {
+            System.out.println(deck.deal());
+        }
+    }
+}
